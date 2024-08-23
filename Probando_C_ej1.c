8954b9e6c188606ac14640b5eb227c47048ae9fb
FelipeFidanza <149431982+FelipeFidanza@users.noreply.github.com>
2024-08-23 16:07:32 -0300

    Update Probando_C_ej1.c

--- Probando_C_ej1.c
+++ Probando_C_ej1.c
@@ -1,8 +1,9 @@
-#include <stdio.h> //para poder usar ìprintfî
+#include <stdio.h> //para poder usar ‚Äúprintf‚Äù
 #include <stdlib.h>
 int main() //siempre se ejecuta primero funcion main
 {
 printf("Hola mundo!!\n); //printf muestra por pantalla
+printf("Adios mundo...");
 system("pause");
 return(0); //si todo salio bien, main retorna 0
 }
